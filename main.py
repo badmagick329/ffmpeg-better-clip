6617533e4012fbb22b3cf4971a31b7064b5d8918
badmagick329 <uzairfarooqi@gmail.com>
2025-02-26 11:27:05 +0000

    feat: split multiple commands

--- main.py
+++ main.py
@@ -4,9 +4,13 @@ from src.ffmpeg_cmd import FFmpegCmd
 
 
 def main():
-    ffmpeg_str = pyperclip.paste()
-    ffmpeg_cmd = FFmpegCmd.create(ffmpeg_str)
-    pyperclip.copy(ffmpeg_cmd.to_duration_cmd())
+    ffmpeg_strs = [
+        f"ffmpeg {f.strip()}" for f in pyperclip.paste().split("ffmpeg ") if f.strip()
+    ]
+    cmd_strings = []
+    for ffmpeg_str in ffmpeg_strs:
+        cmd_strings.append(FFmpegCmd.create(ffmpeg_str).to_duration_cmd())
+    pyperclip.copy("\n".join(cmd_strings))
 
 
 if __name__ == "__main__":
